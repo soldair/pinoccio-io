771d0b5c29a042a9a858dcb99e32c54f88c81a5b
Ryan Day <soldair@gmail.com>
2014-05-14 20:11:21 -0700

    toggling digital pins works!

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 var pinoccio = require('pinoccio')
 var Emitter = require("events").EventEmitter;
 var util = require('util');
-var xtend = require('xtend');
 
 // there are more pins but they are already connected to a bunch of cool things.
 // im not sure the right way to manage it. io.led?
 // https://github.com/Pinoccio/core-pinoccio/blob/master/avr/variants/pinoccio/pins_arduino.h
 var pins = [
+  { id: "D0", modes: [-2] }, //always reserved
+  { id: "D1", modes: [-2] }, //always reserved
   { id: "D2", modes: [0, 1, 3, 4] },
   { id: "D3", modes: [0, 1, 3, 4] },
   { id: "D4", modes: [0, 1, 3, 4] },
   { id: "D5", modes: [0, 1, 3, 4] },
-  { id: "D6", modes: [0, 1] },
-  { id: "D7", modes: [0, 1] },
-  { id: "D8", modes: [0, 1] },
+  { id: "D6", modes: [0, 1] }, // reserved on lead scout. sdcard 
+  { id: "D7", modes: [0, 1] }, // reserved on lead scout. wifi
+  { id: "D8", modes: [0, 1] }, // reserved on lead scout. wifi
   { id: "A0", modes: [0, 1, 2] },
   { id: "A1", modes: [0, 1, 2] },
   { id: "A2", modes: [0, 1, 2] },
@@ -24,13 +25,17 @@ var pins = [
   { id: "A7", modes: [0, 1, 2] }
 ];
 
+// 
+//                 -2           -1         0        1         2       3
+//var pinModes = ['reserved', 'disabled', 'float', 'output', 'input', 'pwm'];
 var modes = {
   RESERVED:-2, // pinoccio
   DISABLED:-1, // pinoccio 
-  FLOAT:0, // pinoccio
+  FLOAT:0, // pinoccio 
   INPUT: 2,
   OUTPUT: 1,
-  ANALOG:2, // -io interface looks like setting pin mode to input
+  // im not sure about this here. in spark-io he expects 2 saves 2 to the pins array as the mode for the pin but passes 0 to the board 
+  ANALOG:0, 
   PWM: 3,
   SERVO: 4 // todo
 };
@@ -46,7 +51,7 @@ function PinoccioIO(opts){
  
   Emitter.call(z);
 
-  z._api = pinoccio(opts.token);
+  z._api = pinoccio(opts);
 
   z.troop = opts.troop;
   z.scout = opts.scout;
@@ -55,6 +60,8 @@ function PinoccioIO(opts){
 
     if(err) return z.emit('error',err);
     if(!data) return z.emit('error',new Error('unknown troop or scout'));
+
+
     z.sync = z._api.sync();
 
     z.data = {};// sync data object.
@@ -62,9 +69,8 @@ function PinoccioIO(opts){
     // board is ready after i get  available && digital && analog events.
     // TODO FIND GREAT Way to message when a scout may be off / unavailable 
     var isReady = function(){
-      return (z.data.available && z.data.available.available) && z.data.digital && z.data.analog;
+      return !z.isReady && (z.data.available && z.data.available.available) && z.data.digital && z.data.analog;
     };
-
 
     z.sync.on('data',function(data){
       // i care about 3 api events
@@ -73,18 +79,21 @@ function PinoccioIO(opts){
       // digital:   {"type":"digital","mode":[-1,-1,-1,-1,2,-1,-1],"state":[-1,-1,-1,-1,0,-1,-1],"_t":1396672122237}
       // analog:    {"type":"analog","mode":[-1,-1,-1,-1,-1,-1,-1,-1],"state":[-1,-1,-1,-1,-1,-1,-1,-1],"_t":1396651237836}
       //
-
+      data = data.data;
+      
       if(data.account && data.troop == z.troop && data.scout == z.scout && data.type) {
 
         var key = data.type
         z.data[key] = data.value||data;
         
         if(key == 'digital' || key == 'analog') {
-          var offset = key == 'analog'?7:0;
+          var offset = key == 'analog'?9:2;
           var report = data.value;
-          
+          var skew = key == 'digital'?2:0;
+
           report.mode.forEach(function(mode,i){
-            var value = report.digital.state[i];
+            
+            var value = report.state[i];
             var pin = z.pins[offset+i];
             var change = false;
             if(mode != pin.mode) {
@@ -97,8 +106,8 @@ function PinoccioIO(opts){
               pin.value = value;
             } 
 
-            if(this.isReady) {
-              z.emit(key+'-pin-'+i);
+            if(z.isReady && change) {
+              z.emit(key+'-pin-'+(i+offset));
             }
           });
 
@@ -114,27 +123,30 @@ function PinoccioIO(opts){
 
   }); 
 
-  z.pins = pins.map(function(pin) {
+  z.pins = pins.map(function(pin,i) {
     return {
+      id:pin.id,
       supportedModes: pin.modes,
       mode: -1, // disabled. waiting for push from api.
       value: 0
     };  
   }); 
 
-  this.analogPins = this.pins.slice(7).map(function(pin, i) {
-    return i;
+  this.analogPins = this.pins.slice(9).map(function(pin, i) {
+    return i+9;
   }); 
 
 }
 
 util.inherits(PinoccioIO.prototype,Emitter);
+PinoccioIO.prototype = new Emitter;
 
-xtend(PinoccioIO.prototype,{
+mix(PinoccioIO.prototype,{
   name:"pinoccio-io",
   isReady:false,
   HIGH:1,
   LOW:0,
+  MODES:modes,
   pins:[],
   // handle to api.
   _api:false,
@@ -143,9 +155,12 @@ xtend(PinoccioIO.prototype,{
   pinMode:function(pin,mode){
 
     var p = pinType(pin,'digital');
+
+    if(this.pins[p.i].mode === mode) return this;
+
     this.pins[p.i].mode = mode;
 
-    this.command('pin.setmode('+p.pin+','+mode+')',function(err){
+    this.command('pin.setmode("'+p.pin+'",'+mode+')',function(err){
       if(err) console.log('pin setmode error ',p.pin,mode,err);
     });
 
@@ -180,7 +195,7 @@ xtend(PinoccioIO.prototype,{
     // events.setCycle(digtialEvents (default is 50ms),analogEvents (default is 1000ms),peripheral sampling interval (temp battery etc default 60000ms))
     var z = this;
     z.command("events.setCycle("+digitalInterval+","+analogInterval+","+peripheralInterval+");",function(err,data){
-      if(err) z.emit('interval-error',err)
+      if(err) z.emit('command-error',err)
       if(cb) cb(err,data);
       else console.error('error setting sampling interval. ',err);
     });
@@ -201,20 +216,24 @@ xtend(PinoccioIO.prototype,{
 
     value = +value;
     if(isNaN(value)) return false;
-    this.command('pin.write("'+data.pin+'",'+value+')',function(err){
+    this.command('pin.write("'+data.pin+'",'+value+')',function(err,data){
       if(err) console.error('error writing pin ',data,value,err);
     }); 
       
   },
   _pinRead:function(data,handler){
     // expect pin as string
-    this.on(data.type+'-pin-'+data.i,handler);
+    console.log('adding event handler',data.name+'-pin-'+data.i);
+    this.on(data.name+'-pin-'+data.i,handler);
     return this;
   },
   // pinoccio only.
   // send scout script command directly to the scout.
   command:function(command,cb){
-    this._api.rest({url:'/v1/'+this.troop+'/'+this.scout+'/command',data:{command:command}},cb);
+    var z = this;
+    z._api.rest({url:'/v1/'+this.troop+'/'+this.scout+'/command',data:{command:command}},function(err,data){
+      cb(err,data);
+    });
   },
 });
 
@@ -228,16 +247,23 @@ function safeInt(interval,min,max){
 }
 
 function pinType(pin,type){
-  var t = type == 'analog'?'A':'D';
+  var t = type == 'analog'?'a':'d';
   if(typeof pin == 'number'){
-    if(pin >= 7){
-      t = 'A';
-      pin -= 7;
+    if(pin >= 9){
+      t = 'a';
+      pin -= 9;
     }
     pin = t+pin;
   }
 
-  pinInt = (pin.replace(/A|D/i, "") | 0) + (t == 'a' ? 7 : 0);
+  pinInt = (pin.replace(/A|D/i, "") | 0) + (t == 'a' ? 9 : 0);
 
-  return {pin:pin,type:t,i:pinInt};
+  return {pin:pin.toLowerCase(),type:t,name:type,i:pinInt};
+}
+
+
+function mix(o1,o2){
+  for(var i in o2){
+    if(o2.hasOwnProperty(i)) o1[i] = o2[i];
+  }
 }
---
+++ test/test.js
@@ -0,0 +1,44 @@
+var test = require('tape');
+var Pinoccio = require('../');
+
+test("can use board",function(){
+
+  var board = new Pinoccio({
+    token: "71933a35bd8fa564be3096bcdb815061", // todo integrate other tokens.
+    troop:2,
+    scout:1,
+    api:'https://localhost:8003'
+  });
+
+  board.on("ready", function() {
+
+    var z = this;
+
+    this.pinMode("D5", this.MODES.OUTPUT);
+
+    var byte = 0;
+
+    this.digitalWrite("D5", (byte ^= 1));
+    setTimeout(function(){
+      this.digitalWrite("D5", (byte ^= 1));
+    }.bind(this),500)
+
+    var c = 0;
+
+    this.digitalRead("D5",function(){
+
+      if(++c === 1) t.ok(z.pins[5].value,0,'detects off');
+      else t.ok(z.pins[5].value,1,'detects on');
+
+      if(c == 2) t.end();
+
+    });
+
+  }).on('error',function(err){
+    t.error(err,'should not have error');
+  });
+
+});
+
+
+
